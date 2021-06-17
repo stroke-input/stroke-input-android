a7f0ebe1d616ffd6e54932cff636ebbc16e5a755
Conway <yawnoc@pm.me>
2021-06-17 21:48:01 +0800

    Add functionality to switch keyboard button

--- app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
@@ -3,12 +3,37 @@ package io.github.yawnoc.strokeinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-  
+public class MainActivity
+  extends AppCompatActivity
+  implements View.OnClickListener
+{
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    Button switchKeyboardButton =
+      (Button) findViewById(R.id.switch_keyboard_button);
+    switchKeyboardButton.setOnClickListener(this);
+    
   }
+  
+  @Override
+  public void onClick(View view) {
+    
+    int viewId = view.getId();
+    
+    if (viewId == R.id.switch_keyboard_button) {
+      InputMethodManager inputMethodManager =
+        (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+      inputMethodManager.showInputMethodPicker();
+    }
+    
+  }
+  
 }
