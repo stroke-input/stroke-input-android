411687322353e36718fcd327a878d0b70f212992
Conway <yawnoc@pm.me>
2021-06-19 18:33:57 +0800

    Apply GPL-3.0-only to MainActivity.java

--- app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
 package io.github.yawnoc.strokeinput;
 
 import androidx.appcompat.app.AppCompatActivity;
