68d5e8e817b755fbe860cd92dc54dc48039921dc
Conway <yawnoc@pm.me>
2021-06-19 19:01:12 +0800

    Declare input method in the manifest
    Note that at this stage, the keyboard is literally nothing.

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/StrokeInputService.java
@@ -0,0 +1,13 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.inputmethodservice.InputMethodService;
+
+public class StrokeInputService extends InputMethodService {
+}
