341dc61477de7b5a88c40e83f45344f9a81119f8
Conway <yawnoc@pm.me>
2021-06-25 15:13:19 +0800

    Create class InputContainer that extends View

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/InputContainer.java
@@ -0,0 +1,25 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.view.View;
+
+/*
+  A container that holds:
+  - TODO: Candidates bar
+  - TODO: Keyboard
+*/
+public class InputContainer
+  extends View
+{
+  public InputContainer(Context context) {
+    super(context);
+  }
+  
+}
