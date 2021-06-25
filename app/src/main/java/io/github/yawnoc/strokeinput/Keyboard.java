f79603cf32a7bd57d0a4d8bc180ffc13b549d7b2
Conway <yawnoc@pm.me>
2021-06-25 16:00:46 +0800

    Create class Keyboard
    Contains literally nothing yet.

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/Keyboard.java
@@ -0,0 +1,14 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+/*
+  A keyboard of rows of keys, which are to be declared in a layout XML.
+*/
+public class Keyboard {
+}
