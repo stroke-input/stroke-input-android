4ebcfb6a9c55fed05a5821c77ba47e58ff051b5e
Conway <yawnoc@pm.me>
2021-06-26 19:20:54 +0800

    Remove InputContainer unused Drawable import
    Unused since 84f648a0c32

--- app/src/main/java/io/github/yawnoc/strokeinput/InputContainer.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/InputContainer.java
@@ -18,7 +18,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
