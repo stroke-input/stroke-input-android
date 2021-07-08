22bc4f980d50f82527a3fb10c7ee4de558b1c583
Conway <yawnoc@pm.me>
2021-07-09 01:35:52 +0800

    Remove Button import in MainActivity

--- app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 
 /*
   The main activity of the application.
