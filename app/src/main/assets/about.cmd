ac264ff3d4e645aa316ca8a2011c5eead4c9bcbf
Conway <yawnoc@pm.me>
2021-07-12 01:34:55 +0800

    Create asset about.html (from about.cmd)

---
+++ app/src/main/assets/about.cmd
@@ -0,0 +1,60 @@
+<##
+  To be compiled with Conway-Markdown (CMD),
+  see <https://github.com/conway-markdown/conway-markdown>.
+##>
+%%
+  %title About
+  %css
+    body {
+      font-family: sans-serif;
+      padding: 0.5em;
+    }
+    h1 {
+      font-size: 1.7em;
+      margin-top: 0.3em;
+    }
+    h2 {
+      font-size: 1.4em;
+    }
+    h3 {
+      font-size: 1.3em;
+    }
+    .notice {
+      border: 1px solid black;
+      padding: 0.3em;
+    }
+%%
+
+
+# Stroke Input Method (0.4.0) #
+
+----{.notice}
+©~2021~Conway \+
+Licensed under GPL-3.0-only, see \GPL-3.0-only. \+
+----
+----
+Contains bytes copied from the deprecated
+[`Keyboard.java`] and [`KeyboardView.java`],
+which are:
+----
+----{.notice}
+©~2020 The Android Open Source Project \+
+Licensed under Apache-2.0, see \Apache-2.0. \+
+----
+
+
+<## Deprecated classes ##>
+@@[`Keyboard.java`]
+  https://android.googlesource.com/platform/frameworks/base/+/\
+    33f921769531968a3ba9bc73fb2410f95868cb8d/\
+    core/java/android/inputmethodservice/Keyboard.java
+@@
+@@[`KeyboardView.java`]
+  https://android.googlesource.com/platform/frameworks/base/+/\
+    33f921769531968a3ba9bc73fb2410f95868cb8d/\
+    core/java/android/inputmethodservice/KeyboardView.java
+@@
+
+<## Licenses ##>
+{: \GPL-3.0-only : b<https://www.gnu.org/licenses/> :}
+{: \Apache-2.0 : b<https://www.apache.org/licenses/LICENSE-2.0.html> :}
