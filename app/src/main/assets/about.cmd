80d767569a50ea52ce421811b3a1c442996b07fb
Conway <yawnoc@pm.me>
2021-07-12 01:44:48 +0800

    Add Build dependency notices to about.html

--- app/src/main/assets/about.cmd
+++ app/src/main/assets/about.cmd
@@ -15,9 +15,10 @@
     }
     h2 {
       font-size: 1.4em;
+      margin-top: 1.5em;
     }
     h3 {
-      font-size: 1.3em;
+      font-size: 1.1em;
     }
     .notice {
       border: 1px solid black;
@@ -43,6 +44,27 @@ Licensed under Apache-2.0, see \Apache-2.0. \+
 ----
 
 
+## Build dependencies ##
+
+### [Android AppCompat Library] (1.3.0) ###
+----{.notice}
+©~2021 The Android Open Source Project \+
+Licensed under Apache-2.0, see \Apache-2.0. \+
+----
+
+### [Android ConstraintLayout] (2.0.4) ###
+----{.notice}
+©~2021 The Android Open Source Project \+
+Licensed under Apache-2.0, see \Apache-2.0. \+
+----
+
+### [Material Components For Android] (1.4.0) ###
+----{.notice}
+©~2021 The Android Open Source Project \+
+Licensed under Apache-2.0, see \Apache-2.0. \+
+----
+
+
 <## Deprecated classes ##>
 @@[`Keyboard.java`]
   https://android.googlesource.com/platform/frameworks/base/+/\
@@ -53,6 +75,17 @@ Licensed under Apache-2.0, see \Apache-2.0. \+
   https://android.googlesource.com/platform/frameworks/base/+/\
     33f921769531968a3ba9bc73fb2410f95868cb8d/\
     core/java/android/inputmethodservice/KeyboardView.java
+@@
+
+<## Build dependencies ##>
+@@[Android AppCompat Library]
+  https://mvnrepository.com/artifact/androidx.appcompat/appcompat
+@@
+@@[Android ConstraintLayout]
+  https://mvnrepository.com/artifact/androidx.constraintlayout/constraintlayout
+@@
+@@[Material Components For Android]
+  https://mvnrepository.com/artifact/com.google.android.material/material
 @@
 
 <## Licenses ##>
