9e86a825c6e1a5b2c4e5c77e669313644748578b
Conway <yawnoc@pm.me>
2021-07-20 01:22:14 +0800

    Add TW translation of Help

---
+++ app/src/main/assets/help-zh-Hant-TW.cmd
@@ -0,0 +1,62 @@
+{+ links.txt +}
+{+ lang.txt +}
+
+%%
+  %lang zh-Hant-HK
+  %title 說明
+  %resources <link rel="stylesheet" href="webview.css">
+%%
+
+
+# %title #
+
+
+## 筆畫 ##
+
+''''
+==
+  , <kbd class="stroke-key">㇐</kbd>
+  , 橫、提
+==
+  , <kbd class="stroke-key">㇑</kbd>
+  , 豎、豎鉤
+==
+  , <kbd class="stroke-key">㇒</kbd>
+  , 撇
+==
+  , <kbd class="stroke-key">㇔</kbd>
+  , 捺、點
+==
+  , <kbd class="stroke-key">㇖</kbd>
+  , 折、彎
+''''
+
+
+## 雜項 ##
+
+{: \space-bar: <kbd class="space-bar">◀ 中文 ▶</kbd> :}
+{: \about-key: <kbd>🛈</kbd> :}
+
+''''
+==
+  , 橫掃 \space-bar
+  , 轉語言
+==
+  , 長按 \space-bar
+  , 轉其他鍵盤
+==
+  , 長按 \about-key
+  , 測試專用
+''''
+
+
+<footer>
+  此頁之 [CMD] 源碼： [`help-zh-Hant-TW.cmd`]
+</footer>
+
+
+@@[`help-zh-Hant-TW.cmd`]
+  https://github.com/stroke-input/stroke-input-android/\
+    blob/master/\
+    app/src/main/assets/help-zh-Hant-TW.cmd
+@@
