18bd4475f38147b880a01e837ea1eac0df77fcc2
Conway <yawnoc@pm.me>
2021-07-20 01:07:45 +0800

    Add HK translation of Help

---
+++ app/src/main/assets/help-zh-Hant-HK.cmd
@@ -0,0 +1,62 @@
+{+ links.txt +}
+{+ lang.txt +}
+
+%%
+  %lang zh-Hant-HK
+  %title 説明
+  %resources <link rel="stylesheet" href="webview.css">
+%%
+
+
+# %title #
+
+
+## 筆畫 ##
+
+''''
+==
+  , <kbd class="stroke-key">㇐</kbd>
+  , 畫、提
+==
+  , <kbd class="stroke-key">㇑</kbd>
+  , 棟、棟鉤
+==
+  , <kbd class="stroke-key">㇒</kbd>
+  , 撇
+==
+  , <kbd class="stroke-key">㇔</kbd>
+  , 捺、點
+==
+  , <kbd class="stroke-key">㇖</kbd>
+  , 折、彎
+''''
+
+
+## 雜項 ##
+
+{: \space-bar: <kbd class="space-bar">◀ 中文 ▶</kbd> :}
+{: \about-key: <kbd>🛈</kbd> :}
+
+''''
+==
+  , 橫掃 \space-bar
+  , 轉語言
+==
+  , 長撳 \space-bar
+  , 轉其他鍵盤
+==
+  , 長撳 \about-key
+  , 測試專用
+''''
+
+
+<footer>
+  此頁之 [CMD] 源：[`help.cmd`]
+</footer>
+
+
+@@[`help.cmd`]
+  https://github.com/stroke-input/stroke-input-android/\
+    blob/master/\
+    app/src/main/assets/help.cmd
+@@
