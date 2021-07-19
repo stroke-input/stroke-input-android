b2794196fd3e1538ba8fc81f77270a3b62aa251e
Conway <yawnoc@pm.me>
2021-07-19 22:41:26 +0800

    Create help.cmd and help.html

---
+++ app/src/main/assets/help.cmd
@@ -0,0 +1,58 @@
+%%
+  %title Help
+  %resources <link rel="stylesheet" href="strokes.css">
+%%
+
+
+# %title #
+
+
+## Strokes ##
+
+''''
+==
+  , <kbd lang="zh-Hant">㇐</kbd>
+  , horizontal; raise
+==
+  , <kbd lang="zh-Hant">㇑</kbd>
+  , vertical; vertical-with-hook
+==
+  , <kbd lang="zh-Hant">㇒</kbd>
+  , left-slash
+==
+  , <kbd lang="zh-Hant">㇔</kbd>
+  , right-press; dot
+==
+  , <kbd lang="zh-Hant">㇖</kbd>
+  , turn; bend
+''''
+
+
+## Miscellaneous ##
+
+{: \space-bar: <kbd class="space-bar">◀ English ▶</kbd> :}
+{: \about-key: <kbd>🛈</kbd> :}
+
+''''
+==
+  , Horizontally swipe \space-bar
+  , Change language
+==
+  , Long press \space-bar
+  , Change to other keyboard
+==
+  , Long press \about-key
+  , Testing use
+''''
+
+
+<footer>
+  This page's [CMD] source: [`help.cmd`]
+</footer>
+
+
+@@[`help.cmd`]
+  https://github.com/stroke-input/stroke-input-android/\
+    blob/master/\
+    app/src/main/assets/help.cmd
+@@
