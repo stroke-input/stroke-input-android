11cb4be5f2db08a4b7ef52fd5de7c3471e824a92
Conway <yawnoc@pm.me>
2021-07-23 17:55:25 +0800

    Create new class KeyPreview extending View

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/KeyPreview.java
@@ -0,0 +1,19 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.view.View;
+
+public class KeyPreview extends View {
+  
+  public KeyPreview(final Context context) {
+    super(context);
+  }
+  
+}
