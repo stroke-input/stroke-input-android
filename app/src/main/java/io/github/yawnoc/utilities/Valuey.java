0e567085fd1ec6311d944c8734ff2bbe5fd2a319
Conway <yawnoc@pm.me>
2021-07-23 17:00:09 +0800

    Fix class Valuey missing AOSP Apache 2.0 notice
    Method getDimensionOrFraction is derived from Keyboard.java.

--- app/src/main/java/io/github/yawnoc/utilities/Valuey.java
+++ app/src/main/java/io/github/yawnoc/utilities/Valuey.java
@@ -4,6 +4,19 @@
   This is free software with NO WARRANTY etc. etc.,
   see LICENSE or <https://www.gnu.org/licenses/>.
 */
+/*
+  This file contains bytes copied from the deprecated `Keyboard` class,
+  i.e. `core/java/android/inputmethodservice/Keyboard.java`
+  from <https://android.googlesource.com/platform/frameworks/base>,
+  which is licensed under the Apache License 2.0,
+  see <https://www.apache.org/licenses/LICENSE-2.0.html>.
+  ---
+  Take your pick from the following out-of-date notices:
+  In `core/java/android/inputmethodservice/Keyboard.java`:
+    Copyright (C) 2008-2009 Google Inc.
+  In `NOTICE`:
+    Copyright 2005-2008 The Android Open Source Project
+*/
 
 package io.github.yawnoc.utilities;
 
