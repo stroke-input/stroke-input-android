99d5560d256bf6784ea378a9095874f8dc5796e5
Conway <yawnoc@pm.me>
2021-07-28 22:42:06 +0800

    Create new class KeyPreviewPlane extending View
    The existing implementation of key preview popups is laggy and crappy,
    as discussed in the merge commit 8b17b22.
    This commit begins a new approach:
    instead of having KeyPreview being a View with dimensions equal to the popup
    (which needs to be resized and repositioned whenever activeKey changes),
    we have a KeyPreviewPlane being a View that covers the whole keyboard.

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/KeyPreviewPlane.java
@@ -0,0 +1,22 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.view.View;
+
+/*
+  A plane for key previews, to be displayed in a PopupWindow.
+*/
+public class KeyPreviewPlane extends View {
+  
+  public KeyPreviewPlane(final Context context) {
+    super(context);
+  }
+  
+}
