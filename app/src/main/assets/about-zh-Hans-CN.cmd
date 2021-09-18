04ae46e79f20961d7d8e3b18d35df8ce5706dabd
Conway <yawnoc@pm.me>
2021-09-19 03:03:39 +0800

    Add simplified versions of About and Help

---
+++ app/src/main/assets/about-zh-Hans-CN.cmd
@@ -0,0 +1,77 @@
+{+ links.inc +}
+{+ lang.inc +}
+
+%%
+  %lang zh-Hans-CN
+  %title 关于
+  %resources <link rel="stylesheet" href="webview.css">
+%%
+
+
+# 笔画输入法 (\stroke-input-version) #
+
+----
+__[笔画输入法]__是自由软件，恕无担保。
+----
+----{.notice}
+©~2021~{Conway} \+
+以 {GPL-3.0-only} 授权，见\GPL-3.0-only。 \+
+----
+----
+当中含有从废类 [`Keyboard.java`]、[`KeyboardView.java`] 所抄的字节，即：
+----
+----{.notice}
+©~2020 {Android} 开源项目 \+
+以 {Apache-2.0} 授权，见\Apache-2.0。 \+
+----
+
+
+## 建体所靠的东西 ##
+
+### [Material Components For Android] (\material-version) ###
+----{.notice}
+©~2021 {Android} 开源项目 \+
+以 {Apache-2.0} 授权，见\Apache-2.0。 \+
+----
+
+
+## 笔画输入资料 ##
+
+### [Conway Stroke Data] (\stroke-data-version) ###
+
+#### `phrases-traditional.txt` \+ `phrases-simplified.txt` ####
+----{.notice}
+献以为公，见\CC0。
+----
+
+#### `ranking-traditional.txt` \+ `ranking-simplified.txt` ####
+----{.notice}
+献以为公，见\CC0。
+----
+
+#### `sequence-characters.txt` ####
+----{.notice}
+©~2021 Conway \+
+以 {CC-BY-4.0} 授权，见\CC-BY-4.0。 \+
+----
+
+
+## 键盘字体 ##
+
+### [Stroke Input Keyboard] (\keyboard-font-version) ###
+----{.notice}
+©~2021 Conway \+
+以 {GPL-3.0-only} 授权，见\GPL-3.0-only。 \+
+----
+----
+修改自 [2015 版 <span class="en">Noto Sans CJK TC</span>]，即：
+----
+----{.notice}
+©~2015 谷歌、等 \+
+以 {Apache-2.0} 授权，见\Apache-2.0。 \+
+----
+
+
+<footer>
+  此页的 [CMD] 源码： \cmd-source
+</footer>
---
+++ app/src/main/assets/help-zh-Hans-CN.cmd
@@ -0,0 +1,50 @@
+{+ links.inc +}
+{+ lang.inc +}
+{+ keys.inc +}
+
+%%
+  %lang zh-Hans-CN
+  %title 说明
+  %resources <link rel="stylesheet" href="webview.css">
+%%
+
+
+# %title #
+
+
+## 笔画 ##
+
+''''
+==
+  , <kbd class="stroke-key">\stroke-1</kbd>
+  , 横、提
+==
+  , <kbd class="stroke-key">\stroke-2</kbd>
+  , 竖、竖钩
+==
+  , <kbd class="stroke-key">\stroke-3</kbd>
+  , 撇
+==
+  , <kbd class="stroke-key">\stroke-4</kbd>
+  , 捺、点
+==
+  , <kbd class="stroke-key">\stroke-5</kbd>
+  , 折、弯
+''''
+
+
+## 杂项 ##
+
+''''
+==
+  , 横扫 \space-bar-zh
+  , 转语言
+==
+  , 长按 \space-bar-zh
+  , 转其他键盘
+''''
+
+
+<footer>
+  此页的 [CMD] 源码： \cmd-source
+</footer>
