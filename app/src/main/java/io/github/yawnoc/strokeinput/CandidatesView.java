6e6669fdc27752bacd33ce56e73d0db56fc91ad0
Conway <yawnoc@pm.me>
2021-11-17 17:43:09 +0800

    Create CandidatesView extending RecyclerView

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/CandidatesView.java
@@ -0,0 +1,23 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CandidatesView
+  extends RecyclerView
+{
+  
+  public CandidatesView(final Context context, final AttributeSet attributes) {
+    super(context, attributes);
+  }
+  
+}
