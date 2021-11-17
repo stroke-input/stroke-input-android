3af998a361bad2a8357de461dd962b1a5310eb04
Conway <yawnoc@pm.me>
2021-11-17 17:27:34 +0800

    Create KeyboardView extending View
    This is to be what InputContainer was, before this rewrite (2a452a4)

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/KeyboardView.java
@@ -0,0 +1,39 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+/*
+  This file contains bytes copied from the deprecated `KeyboardView` class,
+  i.e. `core/java/android/inputmethodservice/KeyboardView.java`
+  from <https://android.googlesource.com/platform/frameworks/base>,
+  which is licensed under the Apache License 2.0,
+  see <https://www.apache.org/licenses/LICENSE-2.0.html>.
+  ---
+  Take your pick from the following out-of-date notices:
+  In `core/java/android/inputmethodservice/KeyboardView.java`:
+    Copyright (C) 2008-2009 Google Inc.
+  In `NOTICE`:
+    Copyright 2005-2008 The Android Open Source Project
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.view.View;
+
+public class KeyboardView
+  extends View
+  implements View.OnClickListener
+{
+  
+  public KeyboardView(final Context context) {
+    super(context);
+  }
+  
+  @Override
+  public void onClick(final View view) {
+  }
+  
+}
