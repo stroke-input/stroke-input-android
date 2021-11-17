7f69b3aa423fc5fa92a83ab441bec2e983049d7a
Conway <yawnoc@pm.me>
2021-11-17 17:23:16 +0800

    Create InputContainer extending LinearLayout

---
+++ app/src/main/java/io/github/yawnoc/strokeinput/InputContainer.java
@@ -0,0 +1,21 @@
+/*
+  Copyright 2021 Conway
+  Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+  This is free software with NO WARRANTY etc. etc.,
+  see LICENSE or <https://www.gnu.org/licenses/>.
+*/
+
+package io.github.yawnoc.strokeinput;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class InputContainer
+  extends LinearLayout
+{
+  
+  public InputContainer(final Context context) {
+    super(context);
+  }
+  
+}
