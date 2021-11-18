607dbf026f7821f6d7d02ead3ce93cb12ad60314
Conway <yawnoc@pm.me>
2021-11-19 02:34:40 +0800

    Remove TODO for 'layout-rewrite'

--- app/src/main/java/io/github/yawnoc/strokeinput/StrokeInputService.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/StrokeInputService.java
@@ -42,7 +42,6 @@ import io.github.yawnoc.utilities.Stringy;
 /*
   An InputMethodService for the Stroke Input Method (筆畫輸入法).
   TODO:
-    - Make the current rewrite (branch 'layout-rewrite') actually work
     - Actually complete the phrase data set
 */
 public class StrokeInputService
