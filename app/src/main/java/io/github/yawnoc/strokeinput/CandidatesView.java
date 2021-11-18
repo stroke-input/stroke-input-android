fef4b7c0d1b85282bd2cdcdf5928c229d34d5832
Conway <yawnoc@pm.me>
2021-11-18 12:39:34 +0800

    Remove unused import in CandidatesView

--- app/src/main/java/io/github/yawnoc/strokeinput/CandidatesView.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/CandidatesView.java
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /*
   A view that holds candidates.
