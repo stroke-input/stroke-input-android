245a9a65f2828129c372756e6569ff9dcd84b947
Conway <yawnoc@pm.me>
2021-11-19 18:31:25 +0800

    Apply style to Stringy.java

--- app/src/main/java/io/github/yawnoc/utilities/Stringy.java
+++ app/src/main/java/io/github/yawnoc/utilities/Stringy.java
@@ -13,77 +13,84 @@ import java.util.Set;
 
 public final class Stringy
 {
-  private Stringy() {
+  private Stringy()
+  {
     // Do not instantiate
   }
   
-  public static boolean isAscii(final String string) {
+  public static boolean isAscii(final String string)
+  {
     return string.matches("\\p{ASCII}*");
   }
   
-  public static String removePrefix(final String prefixRegex, final String string) {
+  public static String removePrefix(final String prefixRegex, final String string)
+  {
     return string.replaceFirst("^" + prefixRegex, "");
   }
   
-  public static String removeSuffix(final String suffixRegex, final String string) {
+  public static String removeSuffix(final String suffixRegex, final String string)
+  {
     return string.replaceFirst(suffixRegex + "$", "");
   }
   
   /*
     Get the first (unicode) code point.
   */
-  public static int getFirstCodePoint(final String string) {
+  public static int getFirstCodePoint(final String string)
+  {
     return string.codePointAt(0);
   }
   
   /*
     Convert a string to a list of (unicode) code points.
   */
-  public static List<Integer> toCodePointList(final String string) {
-    
+  public static List<Integer> toCodePointList(final String string)
+  {
     final List<Integer> codePointList = new ArrayList<>();
     
     final int charCount = string.length();
-    for (int charIndex = 0; charIndex < charCount;) {
+    for (int charIndex = 0; charIndex < charCount;)
+    {
       final int codePoint = string.codePointAt(charIndex);
       codePointList.add(codePoint);
       charIndex += Character.charCount(codePoint);
     }
     
     return codePointList;
-    
   }
   
   /*
     Add the (unicode) code points of a string to a set
   */
-  public static void addCodePointsToSet(final String string, final Set<Integer> set) {
-    
+  public static void addCodePointsToSet(final String string, final Set<Integer> set)
+  {
     final int charCount = string.length();
-    for (int charIndex = 0; charIndex < charCount;) {
+    for (int charIndex = 0; charIndex < charCount;)
+    {
       final int codePoint = string.codePointAt(charIndex);
       set.add(codePoint);
       charIndex += Character.charCount(codePoint);
     }
-    
   }
   
   /*
     Convert a code point to a string
   */
-  public static String toString(final int codePoint) {
+  public static String toString(final int codePoint)
+  {
     return String.valueOf(Character.toChars(codePoint));
   }
   
   /*
     Convert a string to a list of (unicode) characters.
   */
-  public static List<String> toCharacterList(final String string) {
-    
+  public static List<String> toCharacterList(final String string)
+  {
     final List<String> characterList = new ArrayList<>();
     
     final int codePointCount = string.codePointCount(0, string.length());
-    for (int codePointIndex = 0; codePointIndex < codePointCount; codePointIndex++) {
+    for (int codePointIndex = 0; codePointIndex < codePointCount; codePointIndex++)
+    {
       characterList.add(
         string.substring(
           string.offsetByCodePoints(0, codePointIndex),
@@ -93,30 +100,30 @@ public final class Stringy
     }
     
     return characterList;
-    
   }
   
   /*
     Sunder a string into two at the first occurrence of a delimiter.
   */
-  public static String[] sunder(final String string, final String delimiter) {
-    
+  public static String[] sunder(final String string, final String delimiter)
+  {
     final int delimiterIndex = string.indexOf(delimiter);
     final int delimiterLength = delimiter.length();
     
     final String substringBeforeDelimiter;
     final String substringAfterDelimiter;
     
-    if (delimiterIndex < 0) {
+    if (delimiterIndex < 0)
+    {
       substringBeforeDelimiter = string;
       substringAfterDelimiter = "";
     }
-    else {
+    else
+    {
       substringBeforeDelimiter = string.substring(0, delimiterIndex);
       substringAfterDelimiter = string.substring(delimiterIndex + delimiterLength);
     }
     
     return new String[]{substringBeforeDelimiter, substringAfterDelimiter};
-    
   }
 }
