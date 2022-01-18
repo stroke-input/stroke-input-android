877b8d7f9892974bcf3585b4196ef2f54548c884
Conway <yawnoc@pm.me>
2022-01-19 01:13:58 +0800

    Rename ExampleInstrumentedTest to InstrumentedTest

--- app/src/androidTest/java/io/github/yawnoc/strokeinput/InstrumentedTest.java
+++ app/src/androidTest/java/io/github/yawnoc/strokeinput/InstrumentedTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class InstrumentedTest {
   @Test
   public void useAppContext() {
     // Context of the app under test.
