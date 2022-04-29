0ee8b3416d47dc3b14975421ce1bcfec351a0b38
Conway <yawnoc@pm.me>
2022-04-29 22:50:18 +0800

    Rewrite Help zh-Hans-CN in Conway-Markdown v4

--- app/src/main/assets/help-zh-Hans-CN.cmd
+++ app/src/main/assets/help-zh-Hans-CN.cmd
@@ -1,78 +1,81 @@
-{+ links.inc +}
-{+ lang.inc +}
-{+ keys.inc +}
+< links.cmdr
+< keys.cmdr
+< typography.cmdr
 
-%%
-  %lang zh-Hans-CN
-  %title 说明
-  %resources <link rel="stylesheet" href="webview.css">
-%%
+OrdinaryDictionaryReplacement: #boilerplate-properties-override
+- queue_position: BEFORE #boilerplate-properties
+- apply_mode: SIMULTANEOUS
+* %lang --> zh-Hans-CN
+* %title --> 说明
+
+%%%
+
+@LINK_DEFINITIONS
+
+# %title
 
 
-# %title #
-
-
-## 笔画 ##
+## 笔画
 
 ''''
-==
+//
   , <kbd class="stroke-key">\stroke-1</kbd>
   , 横、提
-==
+//
   , <kbd class="stroke-key">\stroke-2</kbd>
   , 竖、竖钩
-==
+//
   , <kbd class="stroke-key">\stroke-3</kbd>
   , 撇
-==
+//
   , <kbd class="stroke-key">\stroke-4</kbd>
   , 捺、点
-==
+//
   , <kbd class="stroke-key">\stroke-5</kbd>
   , 折、弯
 ''''
 
 
-## 例子 ##
+## 例子
 
 ''''
 |^
-==
+//
   ; 字
   ; 笔顺
 |:
-==
+//
   , 天
   ,{.stroke-key}
     \stroke-1\stroke-1\stroke-3\stroke-4
-==
+//
   , 下
   ,{.stroke-key}
     \stroke-1\stroke-2\stroke-4
-==
+//
   , 为
   ,{.stroke-key}
     \stroke-4\stroke-3\stroke-5\stroke-4
-==
+//
   , 公
   ,{.stroke-key}
     \stroke-3\stroke-4\stroke-5\stroke-4
 ''''
 
 
-## 杂项 ##
+## 杂项
 
 ''''
-==
+//
   , 横扫 \space-bar-zh
   , 转语言
-==
+//
   , 长按 \space-bar-zh
   , 转至其他键盘
 ''''
 
 
-## 收回键盘 ##
+## 收回键盘
 
 ----
 用系统__返回按钮__（或__返回手势__）来收回键盘。
@@ -80,5 +83,5 @@
 
 
 <footer>
-  此页的 [CMD] 源码： \cmd-source
+  此页的 [CMD] 源码： [cmd-source]
 </footer>
