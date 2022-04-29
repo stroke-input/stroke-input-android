261eeab1be2cfdff4a36be3a02602e5b9b8f3c77
Conway <yawnoc@pm.me>
2022-04-29 22:43:03 +0800

    Rewrite Help zh-Hant-HK in Conway-Markdown v4

--- app/src/main/assets/help-zh-Hant-HK.cmd
+++ app/src/main/assets/help-zh-Hant-HK.cmd
@@ -1,79 +1,82 @@
-{+ links.inc +}
-{+ lang.inc +}
-{+ keys.inc +}
+< links.cmdr
+< keys.cmdr
+< typography.cmdr
 
-%%
-  %lang zh-Hant-HK
-  %title 説明
-  %resources <link rel="stylesheet" href="webview.css">
-%%
+OrdinaryDictionaryReplacement: #boilerplate-properties-override
+- queue_position: BEFORE #boilerplate-properties
+- apply_mode: SIMULTANEOUS
+* %lang --> zh-Hant-HK
+* %title --> 説明
+
+%%%
+
+@LINK_DEFINITIONS
+
+# %title
 
 
-# %title #
-
-
-## 筆畫 ##
+## 筆畫
 
 ''''
-==
+//
   , <kbd class="stroke-key">\stroke-1</kbd>
   , 畫、提
-==
+//
   , <kbd class="stroke-key">\stroke-2</kbd>
   , 棟、棟鉤
-==
+//
   , <kbd class="stroke-key">\stroke-3</kbd>
   , 撇
-==
+//
   , <kbd class="stroke-key">\stroke-4</kbd>
   , 捺、點
-==
+//
   , <kbd class="stroke-key">\stroke-5</kbd>
   , 折、彎
 ''''
 
 
-## 例子 ##
+## 例子
 
 ''''
 |^
-==
+//
   ; 字
   ; 筆順
 |:
-==
+//
   , 天
   ,{.stroke-key}
     \stroke-1\stroke-1\stroke-3\stroke-4
-==
+//
   , 下
   ,{.stroke-key}
     \stroke-1\stroke-2\stroke-4
-==
+//
   , 為
   ,{.stroke-key}
     \stroke-4\stroke-3\stroke-5\stroke-5\stroke-5\
     \stroke-4\stroke-4\stroke-4\stroke-4
-==
+//
   , 公
   ,{.stroke-key}
     \stroke-3\stroke-4\stroke-5\stroke-4
 ''''
 
 
-## 雜項 ##
+## 雜項
 
 ''''
-==
+//
   , 橫掃 \space-bar-zh
   , 轉語言
-==
+//
   , 長撳 \space-bar-zh
   , 轉至其他鍵盤
 ''''
 
 
-## 收回鍵盤 ##
+## 收回鍵盤
 
 ----
 以系統__返回掣__（或__返回手勢__）而收回鍵盤。
@@ -81,5 +84,5 @@
 
 
 <footer>
-  此頁之 [CMD] 源碼： \cmd-source
+  此頁之 [CMD] 源碼： [cmd-source]
 </footer>
