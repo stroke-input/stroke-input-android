1f52f0a73fd9378183f4777702bc1973e988d470
Conway <yawnoc@pm.me>
2023-04-12 21:47:23 +0800

    Implement keyboard height adjustment SeekBar display text update

--- app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
+++ app/src/main/java/io/github/yawnoc/strokeinput/MainActivity.java
@@ -19,7 +19,10 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 import io.github.yawnoc.utilities.Contexty;
 
@@ -33,6 +36,10 @@ public class MainActivity
   public static final String CANDIDATE_ORDER_PREFERENCE_KEY = "candidateOrderPreference";
   public static final String CANDIDATE_ORDER_PREFER_TRADITIONAL_FIRST = "TRADITIONAL_FIRST";
   public static final String CANDIDATE_ORDER_PREFER_SIMPLIFIED_FIRST = "SIMPLIFIED_FIRST";
+  public static final float KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_MIN = 0.5f;
+  public static final float KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_MAX = 1.5f;
+  public static final float KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_RANGE =
+          KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_MAX - KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_MIN;
   
   private static final String ASSETS_DIRECTORY = "file:///android_asset/";
   private static final String SOURCE_CODE_URI = "https://github.com/stroke-input/stroke-input-android";
@@ -59,6 +66,37 @@ public class MainActivity
     findViewById(R.id.change_keyboard_button).setOnClickListener(this);
     findViewById(R.id.candidate_order_button).setOnClickListener(this);
     
+    final SeekBar keyboardHeightAdjustmentSeekBar = findViewById(R.id.keyboard_height_adjustment_seek_bar);
+    final TextView keyboardHeightAdjustmentDisplayText = findViewById(R.id.keyboard_height_adjustment_display_text);
+    
+    keyboardHeightAdjustmentSeekBar.setOnSeekBarChangeListener(
+      new SeekBar.OnSeekBarChangeListener()
+      {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int newProgress, boolean isUserChange)
+        {
+          final float adjustmentFactor =
+                  KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_MIN
+                  + ((float) newProgress) / seekBar.getMax() * KEYBOARD_HEIGHT_ADJUSTMENT_FACTOR_RANGE;
+          final String adjustmentPercentage = NumberFormat.getPercentInstance().format(adjustmentFactor);
+          keyboardHeightAdjustmentDisplayText.setText(adjustmentPercentage);
+        }
+        
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar)
+        {
+        }
+        
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar)
+        {
+        }
+      }
+    );
+    keyboardHeightAdjustmentSeekBar.setProgress(0); // TODO: use constant
+    keyboardHeightAdjustmentSeekBar.setProgress(10); // TODO: use constant
+    
+    // TODO: set keyboard height adjustment seek bar
     setCandidateOrderButtonText(loadSavedCandidateOrderPreference());
     findViewById(R.id.test_input).requestFocus();
   }
